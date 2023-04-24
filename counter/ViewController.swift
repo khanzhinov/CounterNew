2e814c344a8bfa8d7515eed0370bab6a9f27d4b3
Pavel <khanzhinov@yandex.ru>
2023-04-24 21:45:10 +0300

    тупик

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var ScoreLabel: UILabel!
+    private func configurecounter() {
+     ScoreLabel.text = "0"
+        //return ScoreLabel.text
+    }
+    let result = configurecounter()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
