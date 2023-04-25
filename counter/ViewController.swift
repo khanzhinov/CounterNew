0026365b36b653b2188edcb39cfc50656fc0f131
Pavel <khanzhinov@yandex.ru>
2023-04-25 21:15:17 +0300

    итоговый вариант

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
     //переменные
-    var a = -1
+    
     var Score: Int = 0 {
+        //обновилась переменная или нет, после того как обновилась запускает код
         didSet {
             updateScoreLabel()
         }
@@ -22,22 +25,19 @@ class ViewController: UIViewController {
     @IBOutlet var button: UIButton!
     
     //методы
-    
+    //1. Метод обновления переменной
     private func updateScoreLabel() {
         ScoreLabel.text = "\(Score)"
     }
-    private func schet() {
-        if a == 0 {
-            Score += 1
-        }
-    }
-    private func configurecounter() {
-        ScoreLabel.text = "0"
-    }
-    
+//2. Данный метод вызывается при нажатии на кнопку
     @IBAction func ButtonPushed(_ sender: Any) {
+        print("WIN")
+        
+        
+        Score += 1
         
     }
+    // 3. Метод отображения переменной на экране
     override func viewDidLoad() {
         super.viewDidLoad()
         ScoreLabel.text = "0"
